6a7291501198975ed1e290e1eaa893381c7cb456
noahfranco <noahfra@gmail.com>
2020-01-11 13:07:04 -0500

    finished she said yes compoenent

--- jackie/src/components/SheSaidYes.js
+++ jackie/src/components/SheSaidYes.js
@@ -1,11 +1,17 @@
 import React from "react"; 
+import '../App.css';
 
 class SheSaidYes extends React.Component {
 
     render() {
         return(
             <>
-              <h1> Jackie + Noah </h1> 
+            <div style={{marginTop: "100px"}}> 
+            <h1> Jackie + Noah </h1>
+            <div className="animation"> 
+            <h2 style={{color: "red", fontSize: "180px"}}> ♥️ </h2>  
+            </div> 
+            </div> 
             </>
         )
     }
