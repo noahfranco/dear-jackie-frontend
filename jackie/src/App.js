1729d799b3095f7d4a3e04932a80b5b5d8d99fcd
noahfranco <noahfra@gmail.com>
2020-01-11 12:02:11 -0500

    added router and link App to make mermories component

--- jackie/src/App.js
+++ jackie/src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 import GoogleMap from "./components/Map.js"; 
+import MakeMemories from "./components/MakeMemories.js"; 
 
 function App() {
   return (
     <div className="App">
+     <Router> 
      <h2> Dear Jackie </h2> 
-     <GoogleMap/> 
+     <div style={{marginBottom: "10px"}}> 
+     <Link to="/makememories"> <div style={{textDecoration: "none", color: "black"}}> Make More Memories </div> </Link>
+    </div>
+     <Route exact path="/" render={props => (
+        <GoogleMap {...props} /> 
+      )} />
+
+    <Route path="/makememories" component={MakeMemories} />
+      </Router>
+      
+     {/* <GoogleMap/>  */}
     </div>
   );
 }
---
+++ jackie/src/components/MakeMemories.js
@@ -0,0 +1,14 @@
+import React from "react"; 
+
+class MakeMemories extends React.Component {
+
+    render() {
+        return(
+            <>
+            <div> </div> 
+            </>
+        )
+    }
+}
+
+export default MakeMemories; 
