64c6e233e2117ea3a74aa488fd404a60d8258e58
noahfranco <noahfra@gmail.com>
2020-01-11 12:38:59 -0500

    finished Memories component for now

--- jackie/src/components/MakeMemories.js
+++ jackie/src/components/MakeMemories.js
@@ -1,11 +1,26 @@
 import React from "react"; 
+import {Jumbotron, Button} from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+    
 
 class MakeMemories extends React.Component {
 
     render() {
         return(
             <>
-            <div> </div> 
+                <Jumbotron style={{background: "white"}}> 
+                    <h3> Will you be my girlfirend? </h3>
+                    <div style={{marginTop: "20px"}}>
+
+                    <div style={{padding: "10px"}}> 
+                    <Button variant="primary" style={{fontSize: "70px"}}> Yes </Button>
+                    </div> 
+                    <div style={{padding: "70px"}}> 
+                    <Button variant="primary"> No </Button> 
+                    </div> 
+                    </div>
+                </Jumbotron>
             </>
         )
     }
