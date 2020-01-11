0a22c59356ebed8d84efcab12b26423dbb714e29
noahfranco <noahfra@gmail.com>
2020-01-10 22:19:05 -0500

    added an state to the Map class

---
+++ jackie/src/components/Map.js
@@ -0,0 +1,25 @@
+import React from "react"; 
+import GoogleMapReact from "google-map-react";
+
+class Map extends React.Component  {
+    static latAndLong = {
+        location: {
+            latitude: 35.1954, 
+            longitude: 79.4695
+        },
+        zoom: 11
+    }
+
+render() {
+    return (
+
+        <div> 
+        
+
+        </div>
+
+        )
+    }
+}
+
+export default Map; 
