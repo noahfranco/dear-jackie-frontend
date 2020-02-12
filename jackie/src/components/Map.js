e386038e8d0c19f6c9f5c1d754d841e9ef67ba1f
Noah Franco <noahfranco@jonadabs-mini.lan>
2020-02-12 10:48:29 -0500

    plugging in frontend to backend

--- jackie/src/components/Map.js
+++ jackie/src/components/Map.js
@@ -1,84 +1,95 @@
-import React from "react"; 
+import React, {useEffect} from "react"; 
 import {Map, GoogleApiWrapper, Marker} from "google-maps-react"; 
-// import MapModal from "./MapModal.js";  
+import axios from "axios"
+// import MapModal from "./MapModal.js"; 
 
 class GoogleMap extends React.Component  {
     constructor(props) {
         super(props); 
         this.state = {
             locations: [
-                // First place we ever met in person
                 {
-                    lat: 35.357250,
-                    lng: -79.798050 
-                },
-                // Mall where we spent some time together
-                {
-                    lat: 35.699140, 
-                    lng: -79.798050
-                },
-                // Park we went to play soccer at 
-                {
-                    lat: 35.355970, 
-                    lng: -79.785600
-                }, 
-                // Place where we went and got ice cream
-                {
-                    lat: 35.300980,
-                    lng: -79.665070
-                },
-                // restaurant where we had our first date
-                {
-                    lat: 35.163420,
-                    lng: -79.422250
-                },
-                // Where we went and walked around during our first date
-                {
-                    lat: 35.174049,
-                    lng: -79.390198
-                },
-                // Where we watched our first movie together
-                {
-                    lat: 35.173740,
-                    lng: -79.421593
-                },
-                // Where you watched me play he guitar for the first time
-                {
-                    lat: 35.251560,
-                    lng: -80.737660
-                }, 
-                // Went to TJ-Max 
-                {
-                    lat: 35.157890,
-                    lng: -79.420410
-                },
-                // Went to eat at Chili's
-                {
-                    lat: 35.167600,
-                    lng: -79.424570
-                },
-                // First time trying frozen yogurt
-                {
-                    lat: 35.171700,
-                    lng: -79.423200
-                } 
+                    lat: props.lat,
+                    lng: props.lng
+                }
+                // // First place we ever met in person
+                // {
+                //     lat: 35.357250,
+                //     lng: -79.798050 
+                // },
+                // // Mall where we spent some time together
+                // {
+                //     lat: 35.699140, 
+                //     lng: -79.798050
+                // },
+                // // Park we went to play soccer at 
+                // {
+                //     lat: 35.355970, 
+                //     lng: -79.785600
+                // }, 
+                // // Place where we went and got ice cream
+                // {
+                //     lat: 35.300980,
+                //     lng: -79.665070
+                // },
+                // // restaurant where we had our first date
+                // {
+                //     lat: 35.163420,
+                //     lng: -79.422250
+                // },
+                // // Where we went and walked around during our first date
+                // {
+                //     lat: 35.174049,
+                //     lng: -79.390198
+                // },
+                // // Where we watched our first movie together
+                // {
+                //     lat: 35.173740,
+                //     lng: -79.421593
+                // },
+                // // Where you watched me play he guitar for the first time
+                // {
+                //     lat: 35.251560,
+                //     lng: -80.737660
+                // }, 
+                // // Went to TJ-Max 
+                // {
+                //     lat: 35.157890,
+                //     lng: -79.420410
+                // },
+                // // Went to eat at Chili's
+                // {
+                //     lat: 35.167600,
+                //     lng: -79.424570
+                // },
+                // // First time trying frozen yogurt
+                // {
+                //     lat: 35.171700,
+                //     lng: -79.423200
+                // } 
             ],
         }
     }
 
-// Set up once I get data base running
-    // componentDidMount() {
-        
-    // }
+    componentDidMount() {
+        this.getMap()
+    }
 
-    // getMarker = () => {
-    //     useEffect(() => {
-    //         axios
-    //         .get()
-            
-    //     }, [])
-    // }
-// Set up once I get data base running
+getMap = () => {
+    useEffect(() => {
+        axios
+        .get("localhost:5000/api/event/getMarker", location)
+        .then(res => {
+            console.log("Data from useEffect on Map.js file", res)
+            this.state(res.data)
+        })
+        .catch(error => {
+            console.log(error)
+        })
+    }, [])
+}
+
+   
 
 
     locationFunction = () => {
